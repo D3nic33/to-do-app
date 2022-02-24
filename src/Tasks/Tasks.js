b28101d9f60e54d95be953eaf7221eb479ac5739
Denice van den Steen <vandensteendenice@gmail.com>
2022-02-24 22:28:50 +0100

    add remove function for tasks

--- src/Tasks/Tasks.js
+++ src/Tasks/Tasks.js
@@ -37,8 +37,9 @@ class HandleUserInput extends React.Component {
     let items = this.state.items;
 
     items.push({
+      id: null,
       description: this.state.value,
-      status: null
+      status: null,
     });
 
     this.storeItems(items);
@@ -52,12 +53,22 @@ class HandleUserInput extends React.Component {
       items: items
     });
 
+    console.log(items)
     localStorage.setItem('Tasks', JSON.stringify(this.state.items));
   }
 
   handleClick(index){
     const items = this.state.items;
     items[index].status = 'completed';
+    this.storeItems(items);
+  }
+
+  archiveTask(index){
+    const items = this.state.items;
+    if (index === -1) {
+      return
+    }
+    items.splice(index, 1);
     this.storeItems(items);
   }
 
@@ -81,11 +92,16 @@ class HandleUserInput extends React.Component {
 
           <div className='pt-12 m-auto w-6/12'>
             {this.state.items.map((task, index) => (
-              <div className='py-2' key={index}>
+              <div className='py-2' id={index} key={index}>
                 <div
                   onClick={() => this.handleClick(index)} //(), wordt pas aangeroepen wanneer je event afvuurt
-                  className={`border border-gray-200 rounded-2xl drop-shadow-lg py-5 px-10 ${task.status === 'completed' ? 'bg-lime-100' : 'bg-white'}`}>
+                  className={`flex border border-gray-200 rounded-2xl drop-shadow-lg py-5 px-10 ${task.status === 'completed' ? 'bg-lime-100' : 'bg-white'}`}>
                     {task.description}
+                </div>
+                <div className='flex px-8' onClick={() => this.archiveTask(index)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                  </svg>
                 </div>
               </div>
             ))}
